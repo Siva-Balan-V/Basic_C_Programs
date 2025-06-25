523c872b9f8e896f0b065560fd6a7c2cb5e7e61e
SIVABALAN V <87467271+Siva-Balan-V@users.noreply.github.com>
2025-06-25 23:38:52 +0530

    Create A02_add_two_numbers.c
    To write a C program that Adds two numbers

---
+++ A02_add_two_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n1,n2,sum;
+    printf("Enter number 1 :");
+    scanf("%d",&n1);
+    printf("\nEnter number 2 :");
+    scanf("%d",&n2);
+    sum=n1+n2;
+    printf("\nSum : %d",sum);
+    return 0;
+}
